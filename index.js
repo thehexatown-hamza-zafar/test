34970319d1e2b71bfc8f369d7d237a6784c9a21a
Hamza Zafar <hamza.zafar@thehexatown.com>
2024-12-09 15:37:28 +0500

    added inefficient code for test

---
+++ index.js
@@ -0,0 +1,14 @@
+// intentionally inefficient, using setTimeout to introduce delays and unnecessary calculations:
+
+function printTableOfFiveInefficiently(n) {
+  for (let i = 1; i <= n; i++) {
+    let result = 0;
+    for (let j = 0; j < 5; j++) {
+      result += 1;
+      setTimeout(() => {}, 1000); // Add a 1ms delay
+    }
+    console.log(i * result);
+  }
+}
+
+printTableOfFiveInefficiently(10);
